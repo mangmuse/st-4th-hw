03aea9f8ae0226cbcf40b354f17990ebb4661e5f
mangmuse <mangmangmuse@gmail.com>
2024-05-22 17:34:40 +0900

    ex2

--- src/components/BoxContainer.jsx
+++ src/components/BoxContainer.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
 import styled from "styled-components";
 import Box from "./Box";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -17,8 +17,12 @@ function BoxContainer() {
   return (
     <Container>
       {/* TODO: active prop 을 어떻게 해야 클릭한 박스임을 알수있을 지 active prop에 할당할 값을 수정해보세요. */}
-      {[0, 1, 2, 3, 4].map((index) => (
-        <Box key={index} active={false} onClick={() => handleClick(index)} />
+      {[0, 1, 2, 3, 4].map((_, index) => (
+        <Box
+          key={index}
+          active={activeIndex === index}
+          onClick={() => handleClick(index)}
+        />
       ))}
     </Container>
   );
